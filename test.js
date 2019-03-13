282a1a8df1435f32cad2489d61c6c9ccd4463c19
eviatan <contacto.asoft@gmail.com>
2019-03-12 19:40:17 -0600

    Improving de navbar
    nuestros servicios y tierra de innovadores (transparent y hover)
    
    Falta en collapse & solid
    Icon de los links a redes sociales
    Font-family

--- test.js
+++ test.js
@@ -4,8 +4,11 @@ $(document).ready(function() {
           // checks if window is scrolled more than 500px, adds/removes solid class
           if($(this).scrollTop() > 300) { 
               $('.navbar').addClass('solid');
+              $('.navbar-header').removeClass('navbar-margin-btm-border'); 
+
           } else {
-              $('.navbar').removeClass('solid');
+              $('.navbar').removeClass('solid');  
+              $('.navbar-header').addClass('navbar-margin-btm-border');
           }
         });
 });
